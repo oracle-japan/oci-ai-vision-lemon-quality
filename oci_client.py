6582c743e38d9571ab331724d337f38f430fabcf
tkote <7146253+tkote@users.noreply.github.com>
2023-01-29 07:45:04 +0000

    changed the eval order

--- oci_client.py
+++ oci_client.py
@@ -3,20 +3,22 @@ import oci
 
 logger = logging.getLogger(__name__)
 
+## ~/.oci/config は固定
+## OCI_CONFIG_PROFILE でプロファイルを指定可能
 def get_config_and_signer(file_location=None, profile_name=None):
     config = {}
     signer = None
-    if 'OCI_RESOURCE_PRINCIPAL_VERSION' in os.environ:
-        ver = os.environ['OCI_RESOURCE_PRINCIPAL_VERSION']
-        logger.info(f'Using resource principal signer - OCI_RESOURCE_PRINCIPAL_VERSION={ver}')
-        signer = oci.auth.signers.get_resource_principals_signer()
+    default_profile_name = os.environ.get('OCI_CONFIG_PROFILE') if 'OCI_CONFIG_PROFILE' in os.environ else 'DEFAULT'
+    file_loc = '~/.oci/config' if not file_location else file_location
+    if os.path.isfile(os.path.expanduser(file_loc)):
+        profile = profile_name if profile_name else default_profile_name
+        config = oci.config.from_file(os.path.expanduser(file_loc), profile)
+        logger.info(f'Using config {file_loc} with profile "{profile}"')
     else:
-        default_profile_name = os.environ.get('OCI_CONFIG_PROFILE') if 'OCI_CONFIG_PROFILE' in os.environ else 'DEFAULT'
-        file_loc = '~/.oci/config' if not file_location else file_location
-        if os.path.isfile(os.path.expanduser(file_loc)):
-            profile = profile_name if profile_name else default_profile_name
-            config = oci.config.from_file(os.path.expanduser(file_loc), profile)
-            logger.info(f'Using config {file_loc} with profile {profile}')
+        if 'OCI_RESOURCE_PRINCIPAL_VERSION' in os.environ:
+            ver = os.environ['OCI_RESOURCE_PRINCIPAL_VERSION']
+            logger.info(f'Using resource principal signer - OCI_RESOURCE_PRINCIPAL_VERSION={ver}')
+            signer = oci.auth.signers.get_resource_principals_signer()
         else:
             signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
             logger.info('Using instance principal signer.')
